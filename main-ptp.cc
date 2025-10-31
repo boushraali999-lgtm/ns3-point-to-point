347c8ce12ba9bed7fc32467d8f8285617939336c
boushraali999-lgtm <boushraali999@gmail.com>
2025-10-31 20:35:38 +0100

    Added PointToPoint NS3 simulation code

---
+++ main-ptp.cc
@@ -0,0 +1,80 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/flow-monitor-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("PointToPointExample");
+
+int main(int argc, char *argv[])
+{
+    CommandLine cmd;
+    cmd.Parse(argc, argv);
+
+    Time::SetResolution(Time::NS);
+    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+    NodeContainer nodes;
+    nodes.Create(2);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NetDeviceContainer devices = pointToPoint.Install(nodes);
+
+    InternetStackHelper stack;
+    stack.Install(nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer interfaces = address.Assign(devices);
+
+    UdpEchoServerHelper echoServer(9);
+    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
+
+    UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(10));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
+
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+
+    Simulator::Stop(Seconds(11.0));
+    Simulator::Run();
+
+    monitor->CheckForLostPackets();
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier());
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+
+    for (auto iter = stats.begin(); iter != stats.end(); ++iter)
+    {
+        Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow(iter->first);
+        std::cout << "\nFlow ID: " << iter->first << " (" << t.sourceAddress
+                  << " → " << t.destinationAddress << ")\n";
+        std::cout << "  Tx Packets: " << iter->second.txPackets << "\n";
+        std::cout << "  Rx Packets: " << iter->second.rxPackets << "\n";
+        std::cout << "  Throughput: "
+                  << (iter->second.rxBytes * 8.0 /
+                      (iter->second.timeLastRxPacket.GetSeconds() -
+                       iter->second.timeFirstTxPacket.GetSeconds())) /
+                         1024
+                  << " Kbps\n";
+        std::cout << "  Delay Sum: " << iter->second.delaySum.GetSeconds() << " s\n";
+    }
+
+    Simulator::Destroy();
+    return 0;
+}
+
